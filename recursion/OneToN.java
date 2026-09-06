01ca811a033cf16ea7d0548b7711277f0ee5ace4
Mahi Shukla <mahishukla1207@gmail.com>
2026-09-06 20:41:24 +0530

    Revising recursion

---
+++ recursion/OneToN.java
@@ -0,0 +1,18 @@
+package recursion;
+
+import java.util.*;
+public class OneToN {
+    public static void print(int n){
+        if(n == 0){
+            return;
+        }
+        print(n - 1);
+        System.out.println(n + " ");
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        print(n);
+    }
+}
